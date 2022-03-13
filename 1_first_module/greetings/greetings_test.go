350eb987dcbc9279c90102bd25052db68976b5c3
Tolga Karahan <karahantlg@gmail.com>
2022-03-13 17:31:23 +0300

    added first module in Go

---
+++ 1_first_module/greetings/greetings_test.go
@@ -0,0 +1,23 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Tolga"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello("Tolga")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Tolga") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
+	
