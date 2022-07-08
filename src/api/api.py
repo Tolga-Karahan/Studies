8400f123f9e9b05ea2e4018cc0cf288c849b3d1c
Tolga Karahan <karahantlg@gmail.com>
2022-07-08 21:09:38 +0300

    added users_router

--- src/api/api.py
+++ src/api/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.database.database import engine
 from src.database.models import Base
 from src.routers.posts_router import posts_router
+from src.routers.users_router import users_router
 
 api = FastAPI()
 
@@ -13,3 +14,4 @@ async def root():
     return {'api-version':'0.1.0'}
 
 api.include_router(posts_router)
+api.include_router(users_router)
