560ffbbd97e6c440696e6b0768536f39be3755a1
Tolga Karahan <karahantlg@gmail.com>
2022-07-08 21:34:52 +0300

    fixed typos

--- src/utils/encrypt.py
+++ src/utils/encrypt.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 
-password_context = CryptContext(schemas=['bcrypt'], depreceated='auto')
+password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 def encrypt_password(password: str):
     return password_context.hash(password)
