e69795945d3cba60419c17a188cf02b275afa9b7
Tolga Karahan <karahantlg@gmail.com>
2022-07-08 21:11:56 +0300

    added user related db ops

--- src/database/database_ops.py
+++ src/database/database_ops.py
@@ -1,6 +1,8 @@
-from src.schemas.schemas import PostCreate
-from src.database.models import Posts
+from src.schemas.post_schemas import PostCreate
+from src.schemas.user_schemas import UserCreate
+from src.database.models import Posts, Users
 from sqlalchemy.orm import Session
+from src.utils.encrypt import encrypt_password
 
 def get_posts(db: Session): 
     return db.query(Posts).all()
@@ -37,3 +39,42 @@ def update_post(post_id, updated_post: PostCreate, db: Session):
     db.commit()
 
     return post
+
+def get_users(db: Session): 
+    return db.query(Users).all()
+
+def get_user(user_id: int, db: Session):
+    return db.query(Users).filter(Users.id == user_id).first()
+
+def create_user(user_info: UserCreate, db: Session):
+    encrypted_password = encrypt_password(user_info.password)
+    new_user.password = encrypted_password
+
+    new_user = Users(**user_info.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+def delete_user(user_id: int, db: Session):
+    user = db.query(Users).filter(Users.id == user_id).first()
+
+    if user == None:
+        return False
+    
+    user.delete(synchronize_session=False)
+    db.commit()
+
+    return True
+
+def update_user(user_id, updated_user: UserCreate, db: Session):
+    user = db.query(Users).filter(Users.id == user_id).first()
+
+    if user == None:
+        return
+
+    user.update(updated_user.dict(), synchronize_session=False)
+    db.commit()
+
+    return user
