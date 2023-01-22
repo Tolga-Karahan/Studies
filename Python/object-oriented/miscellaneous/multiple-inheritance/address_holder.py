95cb1474259010635b28ae214791bfb325cf0d7e
Tolga Karahan <karahantlg@gmail.com>
2023-01-22 14:32:41 +0300

    add AddressHolder object

---
+++ Python/object-oriented/miscellaneous/multiple-inheritance/address_holder.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+class AddressHolder:
+    def __init__(self, street: str, city: str, state: str, code: str, **kwargs: Any) -> None:
+        self.street = street
+        self.city = city
+        self.state = state
+        self.code = code
+
+    def __repr__(self):
+        return f"AddressHolder(" \
+               +"{self.street}," \
+               +f"{self.city}," \
+               +f"{self.state}," \
+               +f"{self.code})"
