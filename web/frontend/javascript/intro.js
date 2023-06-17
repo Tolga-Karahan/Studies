315524488054df05237c85484ea511c5ec328153
Tolga <tolga.karahan@hypatos.ai>
2023-06-17 14:23:32 +0300

    add intro js

---
+++ web/frontend/javascript/intro.js
@@ -0,0 +1,10 @@
+// === operator is for evaluating strict equality
+'2' === 2; // False cause one is string another is number
+5 === 2 + 3 // True
+
+// A simple loop
+const fruits = ["apple", "banana", "grape"]
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
