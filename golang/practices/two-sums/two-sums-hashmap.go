97a7439c988a60808c849e13a619fec870fc969d
Tolga <tolga.karahan@hypatos.ai>
2023-06-27 13:52:16 +0300

    purify the solution

--- golang/practices/two-sums/two-sums-hashmap.go
+++ golang/practices/two-sums/two-sums-hashmap.go
@@ -1,28 +1,15 @@
 func twoSum(nums []int, target int) []int {
-    dict := make(map[int][]int)
-
-    for i, v := range nums {
-        if _, ok := dict[v]; !ok {
-            dict[v] = []int{i}
-        } else {
-            dict[v] = append(dict[v], i)
+    numIndices := make(map[int]int)
+    
+    for i, num := range nums {
+        complement := target - num
+        
+        if idx, ok := numIndices[complement]; ok {
+            return []int{idx, i}
         }
+        
+        numIndices[num] = i
     }
-
-    for _, v := range nums {
-        i := dict[v][0]
-
-        if indices, ok := dict[target - v]; ok {
-            j := indices[0]
-            if len(indices) > 1 {
-                j = indices[1]
-            }
-
-            if i != j{
-                return []int{i, j}
-            }
-        }   
-    }
-
+    
     return []int{}
 }
