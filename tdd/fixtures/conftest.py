9fcbc421d39271a1297b0d6622525293ff0c2d2b
Tolga <tolga.karahan@hypatos.ai>
2023-07-26 10:38:33 +0300

    add info

--- tdd/fixtures/conftest.py
+++ tdd/fixtures/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 
-
+# fixtures defined in conftest.py is available
+# across whole directory scope and in subpackages.
+# No need for the import
 @pytest.fixture
 def conftest_fixture():
     return "Conftest Fixture"
 
 
+# request fixture can be used to introspect
+# requesting test function, class or module
 @pytest.fixture
 def introspect_test_context(request):
     module_list = getattr(request.module, "MODULE_LIST")
