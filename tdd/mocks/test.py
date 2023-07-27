bc96cb02549d2d436d22220c04d673a5d48ad385
Tolga <tolga.karahan@hypatos.ai>
2023-07-27 22:52:01 +0300

    add mocking

---
+++ tdd/mocks/test.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+
+from objects import DBEngine, Employee, get_environment, get_time_of_day
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "day, expected",
+    [
+        (datetime(2016, 5, 20, 0, 0, 0), "Night"),
+        (datetime(2016, 5, 20, 1, 10, 0), "Night"),
+        (datetime(2016, 5, 20, 6, 10, 0), "Morning"),
+        (datetime(2016, 5, 20, 12, 0, 0), "Afternoon"),
+        (datetime(2016, 5, 20, 14, 10, 0), "Afternoon"),
+        (datetime(2016, 5, 20, 18, 0, 0), "Evening"),
+        (datetime(2016, 5, 20, 19, 10, 0), "Evening"),
+    ],
+)
+def test_get_time_of_day(day, expected, mocker):
+    # Mock datetime object inside the module
+    # that contains code to be tested
+    date_mock = mocker.patch("objects.datetime")
+    # Mock datetime.now() function used inside
+    # the code block to be tested
+    date_mock.now.return_value = day
+    assert get_time_of_day() == expected
+
+
+# DB mocking example
+def test_engine(mocker):
+    connector_mock = mocker.patch("objects.DBConnector.__init__", return_value=None)
+    mocker.patch("objects.DBEngine.load_data", return_value="abc")
+    engine = DBEngine(
+        connector=connector_mock,
+    )
+
+    assert "abc" == engine.load_data()
+
+
+# API call mocking example
+def test_monthly_schedule(mocker):
+    employee = Employee("Not", "Exist", 5000)
+    mocked_request = mocker.patch("objects.requests.get")
+    mocked_request.return_value.ok = True
+    mocked_request.return_value.text = "Success"
+
+    schedule = employee.monthly_schedule("September")
+    mocked_request.assert_called_with("http://company.com/Exist/September")
+    assert "Success" == schedule
+
+
+# monkeypatch fixture can be used to mock
+# environment related settings
+@pytest.mark.parametrize(
+    "mocked_environment, expected", [("prod", "prod-env"), ("dev", "dev-env")]
+)
+def test_get_environment(mocked_environment, expected, monkeypatch):
+    monkeypatch.setenv("ENVIRONMENT", mocked_environment)
+    assert expected == get_environment()
+
+
+# We can use pytest.raises context manager
+# to test exceptions
+def test_get_environment_exception(monkeypatch):
+    monkeypatch.setenv("ENVIRONMENT", "STAGE")
+
+    with pytest.raises(ValueError, match="Unknown Environment"):
+        get_environment()
