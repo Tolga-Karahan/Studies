d77377ee0eaca5cf31bb9c9b89202587991b75dc
Tolga <tolga.karahan@hypatos.ai>
2023-08-11 15:47:25 +0300

    restructure the directory

--- tests/test_prediction_endpoint.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from adjoe_task.schemas.prediction_schemas import Features
-from adjoe_task.routers.predictions_router import prediction
-
-
-def test_prediction():
-    input = Features(**{"features": [10, 15]})
-    assert {"prediction": 0} == prediction(input)
