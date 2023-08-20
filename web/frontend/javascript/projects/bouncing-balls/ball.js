e0df7e7c3ad81f93d030ce1701aaaa6917a4175d
Tolga <tolga.karahan@hypatos.ai>
2023-08-20 19:34:50 +0300

    convert bouncing-balls to a game

--- web/frontend/javascript/projects/bouncing-balls/ball.js
+++ web/frontend/javascript/projects/bouncing-balls/ball.js
@@ -1,6 +1,6 @@
 import { random, randomRGB } from './utils.js';
 
-class Ball {
+class Shape {
     constructor(x, y, velX, velY, color, size, ctx) {
         this.x = x;
         this.y = y;
@@ -33,7 +33,7 @@ class Ball {
 
     collisionDetect(balls) {
         for (const ball of balls){
-            if (this !== ball) {
+            if (this !== ball && this.exists) {
                 const dx = (this.x - ball.x);
                 const dy = (this.y - ball.y);
                 const distance = Math.sqrt(dx * dx + dy * dy);
@@ -45,5 +45,57 @@ class Ball {
         }
     }
 }
+class Ball extends Shape {
+    constructor(x, y, velX, velY, color, size, ctx) {
+        super(x, y, velX, velY, color, size, ctx);
+        this.exists = true;
+    }
+}
 
-export {Ball};
+class EvilBall extends Shape{
+    constructor(x, y, ctx) {
+        super(x, y, 20, 20, "rgb(255, 255, 255)", 30, ctx);
+        this.killed = 0;
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a":
+                    this.x -= this.velX;
+                    break;
+                case "s":
+                    this.y += this.velY;
+                    break;
+                case "d":
+                    this.x += this.velX;
+                    break;
+                case "w":
+                    this.y -= this.velY;
+                    break;
+            }
+        });
+    }
+
+    draw() {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.color;
+        this.ctx.lineWidth = 3;
+        this.ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        this.ctx.stroke();
+    }
+
+    collisionDetect(balls) {
+        for (const ball of balls){
+            if (ball.exists) {
+                const dx = (this.x - ball.x);
+                const dy = (this.y - ball.y);
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    this.killed++;
+                    ball.exists = false;
+                }
+            }
+        }
+    }
+}
+
+export {Ball, EvilBall};
