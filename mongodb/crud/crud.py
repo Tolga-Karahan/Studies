4ae38dfc2e12b755a742e835256d9bb676ac9131
Tolga <tolga.karahan@hypatos.ai>
2023-09-16 17:42:19 +0300

    add more ops

--- mongodb/crud/crud.py
+++ mongodb/crud/crud.py
@@ -1,30 +1,44 @@
+import uuid
 from pprint import pprint
-import pymongo
+from random import randint, choice
+
 from pymongo import MongoClient
 from pymongo.collection import Collection
-import uuid
 
 
 def connect(client: MongoClient, db: str, collection: str) -> Collection:
     return client.get_database(db).get_collection(collection)
 
 
-def insert_one(connection, data: dict):
+def insert_one(connection: Collection, data: dict):
     connection.insert_one(data)
     return data
 
 
-def find(connection):
+def find_one(connection: Collection, filter: dict, projection: list = None):
+    return connection.find_one(filter=filter, projection=projection)
+
+
+def find_all(connection: Collection):
     return connection.find()
 
 
-def update_one(connection, op: str, data: dict):
+def find_many(connection: Collection, filter: dict, projection: list = None):
+    return connection.find(filter=filter, projection=projection)
+
+
+def replace_one(connection: Collection, filter: dict, data: dict):
+    # Replaces the object with the provided object
+    connection.replace_one(filter=filter, replacement=data)
+
+
+def update_one(connection: Collection, op: str, data: dict, filter: dict = {}):
     # We should provide the operation to update a document
     _update_stmt = {op: data}
-    return connection.update_one(filter={}, update=_update_stmt)
+    return connection.update_one(filter=filter, update=_update_stmt)
 
 
-def delete_one(connection, filter):
+def delete_one(connection: Collection, filter: dict):
     return connection.delete_one(filter=filter)
 
 
@@ -32,14 +46,73 @@ def delete_all(connection):
     return connection.delete_many(filter={})
 
 
-def insert_some_data(connection, n_docs=2):
+def insert_some_data(connection: Collection, n_docs=4):
     for _ in range(n_docs):
         data = {
             "name": "Intro MongoDB",
             "Product ID": str(uuid.uuid4()),
             "type": "tutorial",
+            "qty": randint(0, 100),
         }
         insert_one(connection, data)
+
+
+def insert_some_embed_docs(connection: Collection, n_docs=4):
+    subjects = [
+        "crud",
+        "relations",
+        "security",
+        "Indexes",
+        "Deployment",
+        "Performance",
+        "Architecture",
+        "Drivers",
+    ]
+
+    data = {
+        "Product ID": "7ed036fc-ba8f-4093-b691-60988f267367",
+        "name": "Intro MongoDB",
+        "qty": 55,
+        "type": "tutorial",
+        "details": [
+            {
+                "subject": "crud",
+                "lastUpdated": f"2023{randint(1, 13)}{randint(10, 31)}",
+            },
+            {
+                "subject": choice(subjects),
+                "lastUpdated": f"2023{randint(1, 13)}{randint(10, 31)}",
+            },
+        ],
+    }
+    insert_one(connection, data)
+
+    for _ in range(n_docs - 1):
+        data = {
+            "Product ID": "7ed036fc-ba8f-4093-b691-60988f267367",
+            "name": "Intro MongoDB",
+            "qty": 55,
+            "type": "tutorial",
+            "details": [
+                {
+                    "subject": choice(subjects),
+                    "lastUpdated": f"2023{randint(1, 13)}{randint(10, 31)}",
+                },
+                {
+                    "subject": choice(subjects),
+                    "lastUpdated": f"2023{randint(1, 13)}{randint(10, 31)}",
+                },
+            ],
+        }
+        insert_one(connection, data)
+
+
+def print_many(cursor, msg=None):
+    if msg:
+        print(msg)
+
+    pprint([d for d in cursor])
+    print()
 
 
 if __name__ == "__main__":
@@ -53,25 +126,58 @@ if __name__ == "__main__":
     # Insert some data to work on
     insert_some_data(connection)
 
-    cursor = find(connection)
-    pprint([d for d in cursor])
-    print()
+    cursor = find_all(connection)
+    print_many(cursor, "Initial Docs")
 
     op = "$set"
     data = {"to_delete": True}
     update_one(connection, op, data)
 
     # Check results after update
-    cursor = find(connection)
-    pprint([d for d in cursor])
-    print()
+    cursor = find_all(connection)
+    print_many(cursor, "Docs After Update")
 
     delete_one(connection, filter={"to_delete": True})
 
     # Check results after delete one
-    cursor = find(connection)
-    pprint([d for d in cursor])
-    print()
+    cursor = find_all(connection)
+    print_many(cursor, "Docs After Delete One")
+
+    # Get docs with qty greater than 10
+    cursor = find_many(connection, {"qty": {"$gt": 10}})
+    print_many(cursor, "Docs with qty>10")
+
+    # Get id of the first object and replace it
+    obj_id = find_one(connection, filter={})["_id"]
+    data = {
+        "Product ID": "7ed036fc-ba8f-4093-b691-60988f267367",
+        "name": "Intro MongoDB",
+        "qty": 55,
+        "type": "tutorial",
+    }
+    replace_one(connection, filter={"_id": obj_id}, data=data)
+
+    # Check results after replace one
+    cursor = find_all(connection)
+    print_many(cursor, "Docs After Replace One")
+
+    # Insert some embed docs
+    insert_some_embed_docs(connection)
+
+    # Check docs after insertion of embed docs
+    print_many(find_all(connection), "Docs after inserting embed docs:")
+
+    # Retrieve an embedded doc
+    print("Get embedded doc")
+    pprint(find_one(connection, {"details.subject": "crud"}, projection=["details"]))
+
+    # Update one embed doc with subject crud
+    update_one(
+        connection, "$set", {"details.$.available": False}, {"details.subject": "crud"}
+    )
+
+    # Check docs after updating an embedded doc
+    print_many(find_all(connection), "Docs After Updating An Embedded Doc:")
 
     # Clear the collection
     delete_all(connection)
